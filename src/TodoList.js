f4c2bdab387e8886e26f67302fd3dafa98df20c6
bonivan <ibondarev@luxoft.com>
2022-11-16 09:27:28 -0500

    Adding lifting state and control components

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,31 +1,17 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const todoList = [
-    {
-      id: 1,
-      title: "Visit Paris",
-    },
-    {
-      id: 2,
-      title: "Visit London",
-    },
-    {
-      id: 3,
-      title: "Visit Moscow",
-    },
-];
+function TodoList(props) {
 
-function TodoList() {
-    return (
-      <ul>
-          {todoList.map(function (item) {
-            return (
-              <TodoListItem key={item.id} item={item} />
-            )
-          })}
-      </ul>
-    );
+  return (
+    <ul>
+        {props.todoList.map(function (item) {
+          return (
+            <TodoListItem key={item.id} item={item} />
+          )
+        })}
+    </ul>
+  );
 }
 
 export default TodoList;
