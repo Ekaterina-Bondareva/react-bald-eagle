fb70b397fdd97f4834f09c09ae383898e5ece071
bonivan <ibondarev@luxoft.com>
2022-11-29 13:20:06 -0500

    Adding side-effects custom hooks and fragments

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')));
+
+  React.useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+}
+
 function App() {
 
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo}/>
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
